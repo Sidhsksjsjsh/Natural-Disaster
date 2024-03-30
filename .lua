e67212ea5156b9a501a663883752e8414d870b26
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-03-30 23:00:12 +0900

    Update .lua

--- .lua
+++ .lua
@@ -71,258 +71,164 @@ end
 
 local Social = "Tiktok: @capviktor, YT: Entity, \nMy roblox username Rivanda_Cheater and CookieProgamming (2nd acc)"
 
-local OrionLib = loadstring(game:HttpGet("https://pastebin.com/raw/NMEHkVTb"))()
-local Window = OrionLib:MakeWindow({Name = "VIP Turtle Hub V3",HidePremium = false,SaveConfig = false,ConfigFolder = "OrionTest"})
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sidhsksjsjsh/VAPE-UI-MODDED/main/.lua"))()
+local wndw = lib:Window("VIP Turtle Hub V4")
+local T1 = wndw:Tab("Main")
+local T2 = wndw:Tab("Sound")
+local T3 = wndw:Tab("Character")
+local T4 = wndw:Tab("Teleports")
+local T5 = wndw:Tab("Disaster Log")
 
-local T1 = Window:MakeTab({
-Name = "Main",
-Icon = "rbxassetid://",
-PremiumOnly = false
-})
-
-local T2 = Window:MakeTab({
-Name = "Sound",
-Icon = "rbxassetid://",
-PremiumOnly = false
-})
-
-local T3 = Window:MakeTab({
-Name = "Character",
-Icon = "rbxassetid://",
-PremiumOnly = false
-})
-
-local T4 = Window:MakeTab({
-Name = "Teleports",
-Icon = "rbxassetid://",
-PremiumOnly = false
-})
-
-local T5 = Window:MakeTab({
-Name = "Disaster log",
-Icon = "rbxassetid://",
-PremiumOnly = false
-})
-
-T3:AddSlider({
-  Name = "Speed",
-  Min = 0,
-  Max = 500,
-  Default = 16,
-  Color = Color3.fromRGB(255,255,255),
-  Increment = 1,
-  ValueName = "Speed",
-  Callback = function(Value) 
-    game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed = Value
-  end
-})
+local var = {
+	spamsound = false,
+	mutesound = false,
+	killsound = false,
+	winfarm = false,
+	disaster = {
+		enabled = true,
+		log = false,
+		notify = true,
+		obj = true,
+		annoyui = true
+	},
+	time = {
+		SECOND = math.floor(tick() % 60),
+		MINUTE = math.floor((tick() % 3600) / 60),
+		HOUR = math.floor((tick() % 86400) / 3600),
+		AP = math.floor(tick() % 60),
+		second = 0,
+		minute = 0,
+		hour = 0,
+		ap = "Null"
+	}
+}
+--var.time.second
+T3:Slider("Speed",0,500,16,function(value)
+    game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed = value
+end)
 
 
-T3:AddSlider({
-  Name = "Jump Power",
-  Min = 0,
-  Max = 1000,
-  Default = 50,
-  Color = Color3.fromRGB(255,255,255),
-  Increment = 1,
-  ValueName = "Power",
-  Callback = function(Value)
-    game:GetService("Players").LocalPlayer.Character.Humanoid.JumpPower = Value
-  end
-})
+T3:Slider("Jump",0,1000,50,function(value)
+    game:GetService("Players").LocalPlayer.Character.Humanoid.JumpPower = value
+end)
 
 
-T3:AddSlider({
-  Name = "Visual Game Time",
-  Min = 0,
-  Max = 24,
-  Default = 12,
-  Color = Color3.fromRGB(255,255,255),
-  Increment = 1,
-  ValueName = "Time",
-  Callback = function(Value)
-    game:GetService("Lighting")["ClockTime"] = Value
-  end
-})
+T3:Slider("Visual Game Time",0,24,12,function(value)
+    game:GetService("Lighting")["ClockTime"] = value
+end)
 
-T2:AddToggle({
-  Name = "Spam Sound",
-  Default = false,
-  Callback = function(Value)
-     _G.ClientSound = Value
+T2:Toggle("Spam sound",false,function(value)
+     var.spamsound = value
       
-         while wait() do
-         if _G.ClientSound == false then break end
+     while wait() do
+         if var.spamsound == false then break end
              for i,v in pairs(game.Workspace:GetDescendants()) do
                  if v:IsA("Sound") then 
                     v:Play()
                  end
              end
          end
-    end
-})
+end)
 
-T2:AddToggle({
-  Name = "Mute sound",
-  Default = false,
-  Callback = function(Value)
-     _G.MuteClientSound = Value
+T2:Toggle("Mute sound",false,function(value)
+     var.mutesound = value
       
          while wait() do
-         if _G.MuteClientSound == false then break end
+         if var.mutesound == false then break end
              for i,v in pairs(game.Workspace:GetDescendants()) do
                  if v:IsA("Sound") then 
                     v:Stop()
                  end
               end
          end
-    end
-})
+end)
 
-T2:AddToggle({
-  Name = "Kill sound service",
-  Default = false,
-  Callback = function(Value)
-     _G.ClientSoundService = Value
+T2:Toggle("Kill sound service",false,function(value)
+     var.killsound = value
       
          while wait() do
-         if _G.ClientSoundService == false then break end
+         if var.killsound == false then break end
              Sound1()
              Sound2()
          end
-    end
-})
+    end)
 
-T1:AddToggle({
-  Name = "Enable leak disaster",
-  Default = true,
-  Callback = function(Value)
-    _G.leakenabled = Value
-  end
-})
+T1:Toggle("Enabled disaster",false,function(value)
+    var.disaster.enabled = value
+end)
 
-T1:AddToggle({
-  Name = "Leak disaster [ Notify ]",
-  Default = false,
-  Callback = function(Value)
-    _G.LeakDisasternotify = Value
-  end
-})
+T1:Toggle("Notify disaster",true,function(value)
+    var.disaster.notify = value
+end)
 
-function GetDisasterTime()
-local HOUR = math.floor((tick() % 86400) / 3600)
-local MINUTE = math.floor((tick() % 3600) / 60)
-local SECOND = math.floor(tick() % 60)
-local AP = HOUR > 11 and 'PM' or 'AM'
-HOUR = (HOUR % 12 == 0 and 12 or HOUR % 12)
-HOUR = HOUR < 10 and '0' .. HOUR or HOUR
-MINUTE = MINUTE < 10 and '0' .. MINUTE or MINUTE
-SECOND = SECOND < 10 and '0' .. SECOND or SECOND
-return HOUR .. ":" .. MINUTE .. ":" .. SECOND .. " " .. AP
+local function GetDisasterTime()
+	var.time.hour = (var.time.HOUR % 12 == 0 and 12 or var.time.HOUR % 12)
+	var.time.hour = var.time.HOUR < 10 and '0' .. var.time.HOUR or var.time.HOUR
+	var.time.minute = var.time.MINUTE < 10 and '0' .. var.time.MINUTE or var.time.MINUTE
+	var.time.second = var.time.SECOND < 10 and '0' .. var.time.SECOND or var.time.SECOND
+	--var.time.ap = var.time.HOUR > 11 and 'PM' or 'AM'
+	return var.time.hour .. ":" .. var.time.minute .. ":" .. var.time.second .. " " .. var.time.AP
 end
 
-T1:AddToggle({
-  Name = "Leak disaster [ Log ]",
-  Default = false,
-  Callback = function(Value)
-    _G.LeakLogDisaster = Value
-  end
-})
+T1:Toggle("Log disaster",false,function(value)
+    var.disaster.log = value
+end)
 
-T1:AddToggle({
-  Name = "Safe mode - Removed all disaster objects",
-  Default = false,
-  Callback = function(Value)
-    _G.Safemode = Value
-  end
-})
+T1:Toggle("Remove all disaster object",false,function(value)
+    var.disaster.obj = value
+end)
 
 
-T1:AddToggle({
-  Name = "Remove sandstorm and blizzard UI",
-  Default = false,
-  Callback = function(Value)
-    _G.HideAnnoyUI = Value
-   end
-})
+T1:Toggle("Remove Blizzard and Sandstorm UI",false,function(value)
+    var.disaster.annoyui = value
+end)
 
-T1:AddToggle({
-  Name = "Visual XRAY",
-  Default = true,
-  Callback = function(Value)
-    xraysettings.enab = Value
-    HighlightPlayer()
-   end
-})
-
-T4:AddButton({
-  Name = "Tween TP to lobby",
-  Callback = function()
+T4:Button("Tween to lobby",function()
     TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart,TweenInfo.new(1,Enum.EasingStyle.Linear,Enum.EasingDirection.Out,0,false,0),{CFrame = CFrame.new(-243,194,331)}):Play()
-  end
-})
+end)
 
-T4:AddButton({
-  Name = "Tween TP to map",
-  Callback = function()
+T4:Button("Tween to map",function()
     TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart,TweenInfo.new(1,Enum.EasingStyle.Linear,Enum.EasingDirection.Out,0,false,0),{CFrame = CFrame.new(-117,47,5)}):Play()
-  end
-})
+end)
 
-T4:AddButton({
-  Name = "Tween TP to lobby + spectating other player",
-  Callback = function()
+T4:Button("Tween to lobby ( Outside )",function()
     TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart,TweenInfo.new(1,Enum.EasingStyle.Linear,Enum.EasingDirection.Out,0,false,0),{CFrame = CFrame.new(-240,194,297)}):Play()
-  end
-})
+end)
 
-T4:AddButton({
-  Name = "TP to lobby",
-  Callback = function()
+T4:Button("TP to lobby",function()
     user["Character"]["HumanoidRootPart"]["CFrame"] = CFrame.new(-243,194,331)
-  end
-})
+end)
 
-T4:AddButton({
-  Name = "TP to map",
-  Callback = function()
+T4:Button("TP to map",function()
     user["Character"]["HumanoidRootPart"]["CFrame"] = CFrame.new(-117,47,5)
-  end
-})
+end)
 
-T4:AddButton({
-  Name = "TP to lobby + spectating other player",
-  Callback = function()
+T4:Button("TP to lobby ( Outside )",function()
     user["Character"]["HumanoidRootPart"]["CFrame"] = CFrame.new(-240,194,297)
-  end
-})
+end)
 
-T4:AddToggle({
-  Name = "AFK win farm",
-  Default = false,
-  Callback = function(Value)
-    _G.FFarm = Value
+T4:Toggle("AFK win farm",false,function(value)
+    var.winfarm = Value
       
-         while wait() do
-         if _G.FFarm == false then break end
+     while wait() do
+         if var.winfarm == false then break end
              user["Character"]["HumanoidRootPart"]["CFrame"] = CFrame.new(-269,180,382)
-         end
-   end
-})
+     end
+end)
 
 local logdis = "Disaster will appear if you enabled 'Leak disaster [ Log ]'"
-local forLog = T5:AddParagraph("Disaster Debug",logdis)
-local function disasterPredict()
-if game:GetService("Players")["LocalPlayer"]["Character"]:FindFirstChild("SurvivalTag") and _G.leakenabled == true then
-     if _G.LeakDisasternotify == true then
-        OrionLib:MakeNotification({Name = "Current disaster",Content = game:GetService("Players")["LocalPlayer"]["Character"]["SurvivalTag"]["Value"],Time = 10})
-     elseif _G.LeakLogDisaster == true then
-        logdis = logdis .. "\n" .. game:GetService("Players")["LocalPlayer"]["Character"]["SurvivalTag"]["Value"]
-        forLog:Set(logdis)
-    elseif _G.LeakLogDisaster == true and _G.LeakDisasternotify == true then
-        logdis = logdis .. "\n" .. game:GetService("Players")["LocalPlayer"]["Character"]["SurvivalTag"]["Value"]
-        forLog:Set(logdis)
-        OrionLib:MakeNotification({Name = "Current disaster",Content = game:GetService("Players")["LocalPlayer"]["Character"]["SurvivalTag"]["Value"],Time = 10})
+local forLog = T5:Label(logdis)
+local function disasterPredict() --GetDisasterTime()
+if game:GetService("Players")["LocalPlayer"]["Character"]:FindFirstChild("SurvivalTag") and var.disaster.enabled == true then
+     if var.disaster.notify == true then
+        lib:notify(lib:ColorFonts("⚠️ WARNING ⚠️","Red") .. lib:ColorFonts(" - ","White") .. lib:ColorFonts(user["Character"]["SurvivalTag"]["Value"],"Red") .. lib:ColorFonts(" | " .. GetDisasterTime(),"White"),10)
+     elseif var.disaster.log == true then
+        logdis = logdis .. "\n" .. lib:ColorFonts(user["Character"]["SurvivalTag"]["Value"],"Red") .. lib:ColorFonts(" | " .. GetDisasterTime(),"White")
+        forLog:EditLabel(logdis)
+    elseif var.disaster.log == true and var.disaster.notify == true then
+        logdis = logdis .. "\n" .. lib:ColorFonts(user["Character"]["SurvivalTag"]["Value"],"Red") .. lib:ColorFonts(" | " .. GetDisasterTime(),"White")
+        forLog:EditLabel(logdis)
+        lib:notify(lib:ColorFonts("⚠️ WARNING ⚠️","Red") .. lib:ColorFonts(" | ","White") .. lib:ColorFonts(user["Character"]["SurvivalTag"]["Value"],"Red") .. lib:ColorFonts(" | " .. GetDisasterTime(),"White"),10)
     end
 end
 end
@@ -357,49 +263,49 @@ end
 
 local function onEventAdded(ux)
   ux.ChildAdded:connect(function(event)
-    if event.Name == "TornadoPart" and _G.Safemode == true then
+    if event.Name == "TornadoPart" and var.disaster.obj == true then
         getChild("TornadoPart",function(i,v)
             v:Destroy()
         end)
-      elseif event.Name == "MeteorTemplate" and _G.Safemode == true then
+      elseif event.Name == "MeteorTemplate" and var.disaster.obj == true then
         getChild("MeteorTemplate",function(i,v)
             v:Destroy()
         end)
-      elseif event.Name == "AcidRain" and _G.Safemode == true then
+      elseif event.Name == "AcidRain" and var.disaster.obj == true then
         workspace["Structure"]["AcidRain"]:Destroy()
-      elseif event.Name == "Lightning" and _G.Safemode == true then
+      elseif event.Name == "Lightning" and var.disaster.obj == true then
         getChild("Lightning",function(i,v)
             v:Destroy()
         end)
-      elseif event.Name == "Cloud" and _G.Safemode == true then
+      elseif event.Name == "Cloud" and var.disaster.obj == true then
         workspace["Structure"]["Cloud"]["Mesh"]:Destroy()
-      elseif event.Name == "Dust" and _G.Safemode == true then
+      elseif event.Name == "Dust" and var.disaster.obj == true then
         getChild("Dust",function(i,v)
             v:Destroy()
         end)
-      elseif event.Name == "TsunamiWave" and _G.Safemode == true then
+      elseif event.Name == "TsunamiWave" and var.disaster.obj == true then
         getChild("TsunamiWave",function(i,v)
             v:Destroy()
         end)
-      elseif event.Name == "VirusParticles" and _G.Safemode == true then
+      elseif event.Name == "VirusParticles" and var.disaster.obj == true then
         getChild("VirusParticles",function(i,v)
             v:Destroy()
         end)
-      elseif event.Name == "Lava" and _G.Safemode == true then
+      elseif event.Name == "Lava" and var.disaster.obj == true then
         workspace["Structure"]["Lava"]:Destroy()
-      elseif event.Name == "MeteorFolder" then
+      elseif event.Name == "MeteorFolder" and var.disaster.obj == true then
         getChild("MeteorFolder",function(i,v)
             v:Destroy()
         end)
-      elseif event.Name == "TsunamiBouncy" then
+      elseif event.Name == "TsunamiBouncy" and var.disaster.obj == true then
 	getChild("TsunamiBouncy",function(i,v)
             v:Destroy()
         end)
-      elseif event.Name == "SlimeHolder" then
+      elseif event.Name == "SlimeHolder" and var.disaster.obj == true then
 	getChild("SlimeHolder",function(i,v)
             v:Destroy()
         end)
-      elseif event.Name == "AcidRain" then
+      elseif event.Name == "AcidRain" and var.disaster.obj == true then
 	getChild("AcidRain",function(i,v)
             v:Destroy()
         end)
@@ -409,9 +315,9 @@ end
 
 local function onUIAdded(ux)
   ux.ChildAdded:connect(function(event)
-    if event.Name == "SandStormGui" and _G.HideAnnoyUI == true then
+    if event.Name == "SandStormGui" and var.disaster.annoyui == true then
         game:GetService("Players")["LocalPlayer"]["PlayerGui"]["SandStormGui"]:Destroy()
-      elseif event.Name == "BlizzardGui" and _G.HideAnnoyUI == true then
+      elseif event.Name == "BlizzardGui" and var.disaster.annoyui == true then
         game:GetService("Players")["LocalPlayer"]["PlayerGui"]["BlizzardGui"]:Destroy()
       end
   end)
@@ -419,21 +325,30 @@ end
 
 local function Repeat(R)
    R.ChildAdded:connect(function(Find)
-           if Find.Name == "SurvivalTag" and _G.leakenabled == true then
-            if _G.LeakDisasternotify == true then
-              OrionLib:MakeNotification({Name = "Current disaster",Content = Find.Value,Time = 10})
-            elseif _G.LeakLogDisaster == true then
-              logdis = logdis .. "\n" .. Find.Value
-              forLog:Set(logdis)
-          elseif _G.LeakLogDisaster == true and _G.LeakDisasternotify == true then
-              logdis = logdis .. "\n" .. Find.Value
-              forLog:Set(logdis)
-              OrionLib:MakeNotification({Name = "Current disaster",Content = Find.Value,Time = 10})
+           if Find.Name == "SurvivalTag" and var.disaster.enabled == true then
+            if var.disaster.notify == true then
+              lib:notify(lib:ColorFonts("⚠️ WARNING ⚠️","Red") .. lib:ColorFonts(" | ","White") .. lib:ColorFonts(Find.Value,"Red") .. lib:ColorFonts(" | " .. GetDisasterTime(),"White"),10)
+            elseif var.disaster.log == true then
+              logdis = logdis .. "\n" .. lib:ColorFonts(Find.Value,"Red") .. lib:ColorFonts(" | " .. GetDisasterTime(),"White")
+              forLog:EditLabel(logdis)
+          elseif var.disaster.log == true and var.disaster.notify == true then
+              logdis = logdis .. "\n" .. lib:ColorFonts(Find.Value,"Red") .. lib:ColorFonts(" | " .. GetDisasterTime(),"White")
+              forLog:EditLabel(logdis)
+              lib:notify(lib:ColorFonts("⚠️ WARNING ⚠️","Red") .. lib:ColorFonts(" | ","White") .. lib:ColorFonts(Find.Value,"Red") .. lib:ColorFonts(" | " .. GetDisasterTime(),"White"),10)
             end
            end
        end)
 end
 
+lib:WarnUser(lib:ColorFonts("Im Fall in love with u 😍💕","Pink"))
+
+lib:runtime(function()
+	var.time.HOUR = math.floor((tick() % 86400) / 3600)
+	var.time.MINUTE = math.floor((tick() % 3600) / 60)
+	var.time.SECOND = math.floor(tick() % 60)
+	var.time.AP = var.time.HOUR > 11 and 'PM' or 'AM'
+end)
+	
 Repeat(game:GetService("Players").LocalPlayer.Character)
 disasterPredict()
 onEventAdded(workspace["Structure"])
@@ -441,5 +356,4 @@ instantDelete()
 onUIAdded(game:GetService("Players")["LocalPlayer"]["PlayerGui"])
 game:GetService("Players").LocalPlayer.CharacterAdded:connect(function(R)
        Repeat(R)
-       HighlightPlayer()
 end)
